91583f25d4347718916500cc50e7d9a41f47f8b7
Macoming <zhangzhifang@hinterstellar.com>
2023-07-10 17:32:30 +0800

    迁移数据库，添加未登录用户访问权限

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
 	return render(request, 'learning_logs/index.html')
 
-
+@login_required #限制登录的用户才能访问
 def topics(request):
-	topics = Topic.objects.order_by('date_added')
+	#根据用户过滤自己的topic
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
 
-
+@login_required
 def topic(request, topic_id):
 	"""显示单个主题及其所有的条目"""
 	topic = Topic.objects.get(id=topic_id)
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
 
-
+@login_required
 def new_topic(request):
 	"""添加新主题"""
 	if request.method != 'POST':
@@ -33,11 +38,14 @@ def new_topic(request):
 		#POST提交的数据，对数据进行处理
 		form = TopicForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 	context = {'form':form}
 	return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
 	"""在特定的主题中添加条目"""
 	topic = Topic.objects.get(id=topic_id)
@@ -58,11 +66,13 @@ def new_entry(request, topic_id):
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
 
-
+@login_required
 def edit_entry(request, topic_id):
 	"""编辑条目"""
 	entry = Entry.objects.get(id=topic_id)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 
 	if request.method != 'POST':
 		#初次请求，使用当前条目填充表单
--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -6,10 +6,10 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     #登录页面
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     # 登出
     path('logout/', views.logout_view, name='logout'),
     #注册用户
     path('register/', views.register, name='register'),
-    
+
 ]
