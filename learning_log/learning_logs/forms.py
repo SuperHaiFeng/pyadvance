14db6b884dd039d5b989cfe1dd4465784dba7528
Macoming <zhangzhifang@hinterstellar.com>
2023-07-10 11:43:11 +0800

    添加新建条目和编辑条目

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
+	"""主题表单"""
 	class Meta:
 		model = Topic
 		fields = ['text']
 		labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+	""" 条目表单"""
+	class Meta:
+		model = Entry
+		fields = ['text']
+		labels = {'text': ''}
+		widgets = {'text': forms.Textarea(attrs={'cols': 80})}
--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -14,4 +14,12 @@ urlpatterns = [
 
     #用于添加新主题的页面
     path('new_topic/', views.new_topic, name='new_topic'),
+
+    #用于添加新条目的页面
+    #这个URL模式与形式为http://localhost:8000/new_entry/id/的URL匹配，其中id是一个与主题ID匹配的数字。代码(?P<topic_id>\d+)捕获一个数字值，并将其存储在变量topic_id中。请求的URL与这个模式匹配时，Django将请求和主题ID发送给函数new_entry()。
+    path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    #编辑条目
+    path(r'^edit_entry/(?P<topic_id>\d+)/$', views.edit_entry, name='edit_entry'),
+
 ]
